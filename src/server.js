81e1b1b225f6559161d5f50e1dd32ffce0b5e6e0
nayarix <ilyasskun@gmail.com>
2020-11-15 01:17:33 +0100

    lesson-1 done

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
+
+const port = 3000
 
 app.disable('x-powered-by')
 
@@ -12,4 +15,27 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/', (req, res) => {
+  res.json({ data: 'api' })
+})
+
+router.get('/test', (req, res) => {
+  res.json({ data: 'test' })
+})
+
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.send({ data: 1 })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+  })
+}
