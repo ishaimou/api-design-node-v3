c1284dde156e257c71a484e77ead89d9b3fbd379
nayarix <ilyasskun@gmail.com>
2020-11-16 21:36:54 +0100

    lesson-2 done

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,8 @@
+<<<<<<< Updated upstream
 export default {}
+=======
+import { crudControllers } from '../../utils/crud'
+import { Item } from './item.model'
+
+export default crudControllers(Item)
+>>>>>>> Stashed changes
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,27 @@
+<<<<<<< Updated upstream
 import { Router } from 'express'
 
 const router = Router()
 
 export default router
+=======
+import { Router } from 'express'
+import controllers from './item.controllers'
+
+const router = Router()
+
+// /api/item
+router
+  .route('/')
+  .get(controllers.getMany)
+  .post(controllers.createOne)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne)
+
+export default router
+>>>>>>> Stashed changes
